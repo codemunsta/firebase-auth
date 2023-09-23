918eb001e538bd366e53883b987cbc179c6715bb
Caleb Pedro <click2bundi@gmail.com>
2023-09-23 20:35:57 +0100

    auth update

--- users/custom_authentication.py
+++ users/custom_authentication.py
@@ -12,19 +12,18 @@ class FirebaseAuthentication(authentication.BaseAuthentication):
 
     def authenticate(self, request):
         firebase_id_token = request.META.get('HTTP_AUTHORIZATION')
-        print(firebase_id_token)
         try:
             decoded_token = auth.verify_id_token(firebase_id_token)
-            print(decoded_token)
             uid = decoded_token['uid']
         except auth.ExpiredIdTokenError:
             raise AuthenticationFailed('Auth token Expired')
         except auth.InvalidIdTokenError:
             raise AuthenticationFailed('Invalid Auth token')
+        except Exception as e:
+            raise AuthenticationFailed(str(e))
 
         try:
             user, created = User.objects.get_or_create(email=decoded_token['email'])
-            print(user)
             return user, None
         except Exception as e:
             print(e)
